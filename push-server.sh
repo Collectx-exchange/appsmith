a1372f2ac2d3cf5be73fcd2bd5cfff4a5b523104
Cesar Zagonel <zagonelcesar@gmail.com>
2023-02-02 14:38:32 -0400

    update dockerfile

--- push-server.sh
+++ push-server.sh
@@ -1,4 +1,4 @@
-docker pull appsmith/appsmith-server:v1.8.12
-docker image tag appsmith/appsmith-server:v1.8.12 346284258841.dkr.ecr.us-east-1.amazonaws.com/appsmith-server:v1.8.12
+docker pull appsmith/appsmith-server:v1.9.5
+docker image tag appsmith/appsmith-server:v1.9.5 346284258841.dkr.ecr.us-east-1.amazonaws.com/appsmith-server:v1.9.5
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 346284258841.dkr.ecr.us-east-1.amazonaws.com
-docker image push 346284258841.dkr.ecr.us-east-1.amazonaws.com/appsmith-server:v1.8.12
+docker image push 346284258841.dkr.ecr.us-east-1.amazonaws.com/appsmith-server:v1.9.5
